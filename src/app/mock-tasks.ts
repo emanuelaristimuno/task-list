a8d75290b403ecf0718baae2c4fc35357fdf2476
emanuelaristimuno <emanuelaristimuno@users.noreply.github.com>
2021-12-27 14:17:50 -0300

    agrego mock-list e interface

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,29 @@
+import { Task } from "./Task"
+export const TASKS:Task[]= [
+    {
+        id:1,
+        text:"Terminar Modulo de Angular",
+        day: "01 de Enero de 2022 a las 12:24",
+        reminder: true
+    },
+    {
+        id:2,
+        text:"Terminar Modulo de BD",
+        day: "01 de Enero de 2022 a las 12:25",
+        reminder: true
+    },
+    {
+        id:3,
+        text:"Terminar Modulo de JAVA",
+        day: "01 de Enero de 2022 a las 12:26",
+        reminder: true
+    },
+    {
+        id:4,
+        text:"Terminar Modulo de TESTUniT",
+        day: "01 de Enero de 2022 a las 12:27",
+        reminder: true
+    }
+
+
+]
