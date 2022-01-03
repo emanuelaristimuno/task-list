485ec220a4bcfc5a29d4e4974b4787ae61120367
emanuelaristimuno <emanuelaristimuno@users.noreply.github.com>
2022-01-03 02:34:26 -0300

    agrego uso de json-server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../service/task.service';
 import {Task} from '../../Task';
-import {TASKS} from '../../mock-tasks';
+
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +10,17 @@ import {TASKS} from '../../mock-tasks';
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[]= TASKS;
+  tasks: Task[]= [];
 
-  constructor() { }
+  constructor( private taskServices: TaskService) { 
+   
+  }
 
   ngOnInit(): void {
+    this.taskServices.getTask().subscribe((tasks)=>{
+      this.tasks = tasks;
+     }
+      );
   }
 
   onClick(){
